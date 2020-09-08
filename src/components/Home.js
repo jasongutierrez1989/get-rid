e8facb055afa74fe03dc71ad9fa2b1e0e2ed4938
jasongutierrez1989 <jasongutierrez1989@gmail.com>
2020-09-07 23:00:38 -0700

    basic homepage set up!

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+
+  return (
+    <div className="App">
+          <h1>Get Rid!</h1>
+          <p>Come on, you know you don't need it.</p>
+    </div>
+  );
+};
+
+export default Home;
